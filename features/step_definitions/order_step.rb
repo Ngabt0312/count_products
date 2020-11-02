de9da961e394e0fed3ec5daf29abc45d30a9795c
Ngabt123 <buithuynga0312@gmail.com>
2020-11-02 13:38:18 +0700

    update code

--- features/step_definitions/order_step.rb
+++ features/step_definitions/order_step.rb
@@ -1,21 +1,18 @@
 Given('navigate login page, search and order product') do
-    $driver.get ('https://tiki.vn/')
-    $driver.manage.window.maximize
-  end
+  $driver.get ('https://tiki.vn/')
+  $driver.manage.window.maximize
+end
+
+When('input {string} and order') do |string|
+  $search_bar = $driver.find_element(:css, ".eUnWAD")
+  $search_bar.send_keys(string,:enter)
+  $check_box = $driver.find_element(:css, ".checkbox.is-tikinow")
+  $check_box.click
+  sleep (3)
+  $driver.find_element(:css, ".text-center").click
+end
+
+Then('mess {string}') do |string|
   
-  When('input {string} and order') do |string|
-    $search_bar = $driver.find_element(:css, ".eUnWAD")
-    $search_bar.send_keys(string,:enter)
-    $driver.manage.timeouts.implicit_wait = 5
-    $check_box = $driver.find_element(:css, ".category-filter")
-    $check_box.click
-    sleep (10)
-    #$pop_up = $driver.find_element(:css, ".a-modal-header")
-    #$driver.find_element(:css, ".apply-btn")
-    
-  end
-  
-  Then('mess {string}') do |string|
-    
-  end
+end
   
