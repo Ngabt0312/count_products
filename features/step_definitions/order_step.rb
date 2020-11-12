000f77c75eac75caa51b7695b4431fb1ce8e1001
Ngabt123 <buithuynga0312@gmail.com>
2020-11-12 13:55:00 +0700

    update code

--- features/step_definitions/order_step.rb
+++ features/step_definitions/order_step.rb
@@ -4,7 +4,7 @@ Given('navigate login page, search and order product') do
 end
 When('input {string} and order') do |string|
   #tìm kiếm sản phẩm apple
-  $product_number = "1"
+  $product_number = "2"
   $search_bar = $driver.find_element(:css, ".eUnWAD")
   $search_bar.send_keys(string,:enter)
   $check_box = $driver.find_element(:css, "label.is-tikinow").click #chọn check box Tiki
@@ -23,18 +23,14 @@ When('input {string} and order') do |string|
   $driver.manage.timeouts.implicit_wait = 2.5
   $dropdown.select_by(:text, "Phường Dịch Vọng")
   $driver.find_element(:xpath, ".//button[contains(text(),'GIAO ĐẾN ĐỊA CHỈ NÀY')]").click #tìm theo contains
-  #$driver.find_element(css: ".apply-btn").click #tìm theo css
-  #$driver.find_element(:xpath, "./html/body/div[7]/div/div[3]/div/button").click #tìm theo xpath
+  $driver.manage.timeouts.implicit_wait = 3
   $product_count= $driver.find_elements(:css, "p.title")
   $product_title = $product_count[4].text.delete("...")
   $product_count[4].click
   expect($driver.find_element(:css, "h1.title").text).to include($product_title)
 
   # add to cart
-  $driver.find_element(css: "input.input").send_keys :backspace
-  #$driver.find_element(css: "input.input").send_keys $product_number
-  #$driver.find_element(:css, ".btn.btn-add-to-cart").click
-  sleep (2)
+  #$driver.find_element(:css, "input.input")
 end
 
 Then('check product add to cart') do
